97a5c36a1e561af821ea732749b2d79e1877494f
dil <91594845+bn-u@users.noreply.github.com>
2024-02-18 22:07:53 +0000

    reduces amount of points tracked

--- Assets/Scripts/TrackMovement.cs
+++ Assets/Scripts/TrackMovement.cs
@@ -8,25 +8,24 @@ public class TrackMovement : MonoBehaviour
     private bool tracking = false;
     private Vector3 startPos;
     private Vector3 endPos;
-    private Quaternion startRot;
-    private Quaternion endRot;
 
     public bool right;
     public GameObject targetMesh;
     public bool isDebug = false;
     List<Vector3> pathPoints = new List<Vector3>();
+    List<Vector3> points = new List<Vector3>();
 
     public void EnableTracker()
     {
+        points.Clear();
+        pathPoints.Clear();
         switch (right)
         {
             case true:
-                startPos = GameObject.Find("/XR Interaction Hands Setup/XR Origin (XR Rig)/Camera Offset/Right Hand/Poke Interactor").transform.position;
-                startRot = GameObject.Find("/XR Interaction Hands Setup/XR Origin (XR Rig)/Camera Offset/Right Hand/Poke Interactor").transform.rotation;
+                startPos = RightPos();
                 break;
             case false:
-                startPos = GameObject.Find("/XR Interaction Hands Setup/XR Origin (XR Rig)/Camera Offset/Left Hand/Poke Interactor").transform.position;
-                startRot = GameObject.Find("/XR Interaction Hands Setup/XR Origin (XR Rig)/Camera Offset/Left Hand/Poke Interactor").transform.rotation;
+                startPos = LeftPos();
                 break;
         }
         pathPoints.Add(startPos);
@@ -38,17 +37,29 @@ public class TrackMovement : MonoBehaviour
         switch (right)
         {
             case true:
-                endPos = GameObject.Find("/XR Interaction Hands Setup/XR Origin (XR Rig)/Camera Offset/Right Hand/Poke Interactor").transform.position;
-                endRot = GameObject.Find("/XR Interaction Hands Setup/XR Origin (XR Rig)/Camera Offset/Right Hand/Poke Interactor").transform.rotation;
+                endPos = RightPos();
                 break;
             case false:
-                endPos = GameObject.Find("/XR Interaction Hands Setup/XR Origin (XR Rig)/Camera Offset/Left Hand/Poke Interactor").transform.position;
-                endRot = GameObject.Find("/XR Interaction Hands Setup/XR Origin (XR Rig)/Camera Offset/Left Hand/Poke Interactor").transform.rotation;
+                endPos = LeftPos();
                 break;
         }
         tracking = false;
+        trackPos();
+        pathPoints.Add(endPos);
         createSpline();
     }
+
+    static Vector3 LeftPos()
+    {
+        return GameObject.Find("/XR Interaction Hands Setup/XR Origin (XR Rig)/Camera Offset/Left Hand/Poke Interactor").transform.position;
+    }
+
+    static Vector3 RightPos()
+    {
+        return GameObject.Find("/XR Interaction Hands Setup/XR Origin (XR Rig)/Camera Offset/Right Hand/Poke Interactor").transform.position;
+    }
+
+
 
     private void FindRight()
     {
@@ -75,32 +86,68 @@ public class TrackMovement : MonoBehaviour
                 //right
             }
         }
-        
+
     }
 
     void createSpline()
     {
-        pathPoints.Add(endPos);
-
-        var container = gameObject.AddComponent<SplineContainer>();
+        var container = gameObject.GetComponent<SplineContainer>();
+        if (container == null)
+        {
+            container = gameObject.AddComponent<SplineContainer>();
+        }
+        container.RemoveSpline(container.Spline);
         var spline = container.AddSpline();
         var knots = new BezierKnot[pathPoints.Count];
-
         for (int i = 0; i < pathPoints.Count; i++)
         {
-            knots[i] = new BezierKnot(pathPoints[i], 3 * Vector3.forward, 3 * Vector3.forward);
+            knots[i] = new BezierKnot(pathPoints[i], -0.05f * Vector3.forward, 0.05f * Vector3.forward);
+
+            spline.Knots = knots;
         }
-
-        spline.Knots = knots;
-
     }
 
     void Update()
     {
         if (tracking == true)
         {
-            //pathPoints.Add(1);
+            Vector3 pos;
+            switch (right)
+            {
+                case true:
+                    pos = RightPos();
+                    break;
+                case false:
+                    pos = LeftPos();
+                    break;
+            }
+            points.Add(pos);
         }
     }
+
+    void trackPos()
+    {
+        var result = EveryNthElement(points, 12);
+        for (int i = 0;i < result.Count;i++)
+        {
+            pathPoints.Add(result[i]);
+        }
+        Debug.Log("Points: " + points.Count);
+        Debug.Log("Result: " + result.Count);
+    }
+
+    static List<Vector3> EveryNthElement(List<Vector3> list, int n)
+    {
+        List<Vector3> result = new List<Vector3>();
+        for (int i = 0; i < list.Count; i++)
+        {
+            if ((i % n) == 0)
+            {
+                result.Add(list[i]);
+            }
+        }
+        return result;
+    }
+
 }
 
